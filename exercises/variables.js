b694b06834399732f953ac2ba0cade56f62ea640
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-07-20 11:45:40 +0700

    exercises variables

---
+++ exercises/variables.js
@@ -0,0 +1,37 @@
+// /* ---------Why pay a fortune teller when you can just program your fortune yourself?--------- */
+// var numKids = 5;
+// var partnerName = 'David Becker';
+// var geoLocation = 'New York, NY';
+// var jobTitle = 'developers';
+// var futures = `You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} width ${numKids} kids.`;
+// console.log(futures);
+
+// /* ---------Want to find out how old you'll be? Calculate it!-------- */ 
+// var birthYear = 2000;
+// var currentYear = 2022;
+// var currentAge = currentYear - birthYear;
+// var currentAges = `I will be either ${currentAge - 1} or ${currentAge} in ${currentYear}`;
+// console.log(currentAges);
+
+// /* ---------Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!-------- */
+// var currentAge = 22;
+// var maxAge = 100;
+// var numPerDay = 5;
+// var totalEat = (maxAge - currentAge) * 365 * numPerDay;
+// var output = `You will need ${totalEat}$ to last you until the ripe old age of ${maxAge}`;
+// console.log(output);
+
+// /* -------Calculate properties of a circle-------- */
+// var radius = 2;
+// var circumferenceOfCircle = 2 * Math.PI * radius;
+// console.log(`The circumference is ${circumferenceOfCircle}`);
+// var areaOfCircle = (radius * radius) * Math.PI;
+// console.log(`The area is ${areaOfCircle}`);
+
+// /* ----It's hot out! Let's make a converter based on the steps here.----- */
+// var celsius = 20;
+// var fahrenheit = 68;
+// var convertF = ((celsius / 5) * 9) + 32;
+// var convertC = ((fahrenheit - 32) * 5) / 9;
+// console.log(`${celsius}°C is ${convertF}°F`);
+// console.log(`${fahrenheit}°F is ${convertC}°C`);
