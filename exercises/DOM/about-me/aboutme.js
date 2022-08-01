29139f6325d2ea39ebcb78a8f1f815fd225ba923
Minh Khoi <minhkhoidoan234@gmail.com>
2022-07-31 22:29:33 -0700

    update

---
+++ exercises/DOM/about-me/aboutme.js
@@ -0,0 +1,12 @@
+document.body.style.fontFamily = 'Arial, sans-serif';
+   document.getElementById('nickname').innerHTML = 'Minh Khoi';
+   document.getElementById('favorites').innerHTML = '22';
+   document.getElementById('hometown').innerHTML = 'Da Nang';
+   var items = document.getElementsByTagName('li');
+   for (var i = 0; i < items.length; i++) {
+      items[i].className = 'listitem';
+   }
+
+var myPic = document.createElement('img');
+myPic.src = 'http://gotocon.com/dl/jaoo_aus2008/photos/speakers/Pamela_Fox.jpg';
+document.body.appendChild(myPic);
