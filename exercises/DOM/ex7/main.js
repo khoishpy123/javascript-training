57ce2cd0cd3772ddae5c515246e437b87df815b1
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-08-04 11:13:52 +0700

    exercises 7

--- exercises/DOM/ex7/main.js
+++ exercises/DOM/ex7/main.js
@@ -1,9 +1,11 @@
-function removecolor()
-{
-    var y=document.querySelector('.box');
-    var x=document.getElementById("colorSelect");
-    x.remove(x.selecIndex);
-    y.setAttribute("style", "background-color: red;");
+function removecolor() {
+    var y = document.querySelector('.box');
+    var x = document.getElementById("colorSelect");
+    // x.remove(x.selectedIndex); // remove selection
+    // y.setAttribute("style", "background-color: 'x.value';");
+    y.style.background = x.value;
     y.style.height = '200px';
     y.style.width = '200px';
+    console.log(x.value);
+    console.log(x.options[x.selectedIndex].text);
 }
