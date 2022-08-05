d33c2b44907c57bfb4dcfb0779fcec167cb67d45
Minh Khoi <minhkhoidoan234@gmail.com>
2022-08-04 19:53:10 -0700

    exercises about-me

--- exercises/DOM/about-me/aboutme.js
+++ exercises/DOM/about-me/aboutme.js
@@ -7,7 +7,7 @@ document.body.style.fontFamily = 'Arial, sans-serif';
       items[i].className = 'listitem';
    }
 
-
+   
 var myPic = document.createElement('img');
 myPic.src = 'http://gotocon.com/dl/jaoo_aus2008/photos/speakers/Pamela_Fox.jpg';
 document.body.appendChild(myPic);
