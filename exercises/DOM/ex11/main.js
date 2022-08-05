9abaed3c944ec17462856e5366afecfcd8bf1be3
Minh Khoi <minhkhoidoan234@gmail.com>
2022-08-05 03:20:06 -0700

    exercises 11

--- exercises/DOM/ex11/main.js
+++ exercises/DOM/ex11/main.js
@@ -1,3 +1,17 @@
+function validateform(){
+    var firstName = document.querySelector("#myForm").fname.value;
+    var lastName = document.querySelector("#myForm").lname.value;
+    var email = document.querySelector("#myForm").email.value;
+    var age = document.querySelector("#myForm").age.value;
+
+
+    if(firstName == "" || firstName == null){
+        alert("Please enter a First name!");
+    }
+    else if(firstName != isNaN){
+        alert("Name can not be a number!"); 
+    }
+}
 function fun(){
     document.getElementById("myForm").reset();
 }
