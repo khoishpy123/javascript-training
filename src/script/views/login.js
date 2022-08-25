a1885ed49ee3e4fe1540d364029633900b42d0b4
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-08-25 08:58:26 +0700

    fix format

--- src/script/views/login.js
+++ src/script/views/login.js
@@ -2,35 +2,23 @@ class LoginView {
   constructor() {
     this.btnLogin = document.querySelector(".btn");
   }
-
   bindUser(getUser) {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
     getUser().then((user) => {
-      user.forEach((data) => {
-        if (data.username == username && data.password == password) {
-          alert("dang nhap thanh cong");
-          window.location.href = "./index.html";
-        }
+      user.forEach((item) => {
+        this.btnLogin.addEventListener("click", (e) => {
+          e.preventDefault();
+          let username = document.getElementById("username").value;
+          let password = document.getElementById("password").value;
+
+          if (item.username === username && item.password === password) {
+            alert("Login successful!");
+            window.location.href = "./index.html";
+          } else {
+            alert("UserName and PassWord dont match!");
+          }
+        });
       });
     });
-    // getUser().then((user) => {
-    //   this.btnLogin.addEventListener("click", (e) => {
-    //     e.preventDefault();
-    //     user.forEach((item) => {
-    //       let username = document.getElementById("username").value;
-    //       let password = document.getElementById("password").value;
-
-    //       if (item.username === username && item.password === password) {
-    //         alert("Login successful!");
-    //         window.location.href = "./index.html";
-    //       }else{
-    //         alert("UserName and PassWord dont match!");
-    //       }
-    //     });
-
-    //   });
-    // });
   }
 }
 
