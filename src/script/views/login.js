886760e909ccc6f90ed8c1c08321cec6c43936aa
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-08-25 09:22:03 +0700

    fix error forEach

--- src/script/views/login.js
+++ src/script/views/login.js
@@ -9,12 +9,8 @@ class LoginView {
           e.preventDefault();
           let username = document.getElementById("username").value;
           let password = document.getElementById("password").value;
-
           if (item.username === username && item.password === password) {
-            alert("Login successful!");
             window.location.href = "./index.html";
-          } else {
-            alert("UserName and PassWord dont match!");
           }
         });
       });
