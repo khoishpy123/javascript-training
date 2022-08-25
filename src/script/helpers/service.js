8e503cc6fddbb9574b029aeb88cf6ede309c8683
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-08-25 08:20:48 +0700

    fix error

--- src/script/helpers/service.js
+++ src/script/helpers/service.js
@@ -1,56 +1,56 @@
 import api from "../constants/api.js";
 
-async function request(url, id, method = "GET") {
+async function request(url) {
   const options = {
-    method,
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   };
-
-  if (id) {
-    if (method === "GET") {
-      url += "?" + objectToQueryString(id);
-    } else {
-      options.body = JSON.stringify(id);
-    }
+  const res = await fetch(api.API + url, options);
+  if (!res.ok) {
+    throw new Error("An error has occurred");
   }
-
-  const response = await fetch(api.API + url, options);
-
-  if (response.status !== 200) {
-    return generateErrorResponse(
-      "The server responded with an unexpected status.",
-    );
-  }
-
-  const result = await response.json();
-
+  const result = await res.json();
   return result;
 }
 
-/**
- *
- * @param {object} obj
- * @returns object
- */
-function objectToQueryString(obj) {
-  return Object.keys(obj)
-    .map((key) => key + "=" + obj[key])
-    .join("&");
-}
+  // if (id) {
+  //   if (method === "GET") {
+  //     url += "?" + objectToQueryString(id);
+  //   } else {
+  //     options.body = JSON.stringify(id);
+  //   }
+  // }
+
+  // if (response.status !== 200) {
+  //   return generateErrorResponse(
+  //     "The server responded with an unexpected status.",
+  //   );
+  // }
+
+// /**
+//  *
+//  * @param {object} obj
+//  * @returns object
+//  */
+// function objectToQueryString(obj) {
+//   return Object.keys(obj)
+//     .map((key) => key + "=" + obj[key])
+//     .join("&");
+// }
 
 /**
  *
  * @param {string} message
  * @returns string
  */
-function generateErrorResponse(message) {
-  return {
-    status: "error",
-    message,
-  };
-}
+// function generateErrorResponse(message) {
+//   return {
+//     status: "error",
+//     message,
+//   };
+// }
 
 /**
  * Use url and id to return result request to read data
