1fce8cbac11934f2f4f3885af4fe5e85ba9fe31e
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-09-13 10:41:36 +0700

    render employee list into dashbord

--- src/javascripts/users/controllers.js
+++ src/javascripts/users/controllers.js
@@ -1,1 +1,18 @@
-// add file
+class Controller {
+  constructor(Model, View) {
+    this.Model = Model;
+    this.View = View;
+    this.init();
+  }
+
+  init() {
+    this.handelShowEmployee();
+  }
+
+  async handelShowEmployee() {
+    const employeesList = await this.Model.getEmployees();
+    this.View.displayEmployees(employeesList);
+  }
+}
+
+export default Controller;
