5d7d915cfcdc0de901b7e9bfd4615355ef2abbe6
minhkhoidoan <minhkhoidoan234@gmail.com>
2022-09-13 10:40:28 +0700

    add file main

---
+++ src/javascripts/main/home-main.js
@@ -0,0 +1,6 @@
+import Model from "../users/models.js";
+import View from "../users/views.js";
+import Controller from "../users/controllers.js";
+
+// eslint-disable-next-line no-unused-vars
+const controller = new Controller(new Model(), new View());
